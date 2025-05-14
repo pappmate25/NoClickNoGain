d4bf274857dae943ef176f8ba73aac553209f1c8
Pap Sándor Dániel <pap.sandor.daniel@pm.me>
2025-05-14 13:21:52 +0200

    initial implementation of equations through inheritor test upgrade

---
+++ Assets/Scripts/Equation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+[Serializable]
+public class Equation
+{
+    public static Regex EquationRegex = new Regex(@"^[\d+\s]+$");
+
+    [SerializeField]
+    [HideInInspector]
+    private bool isValid = false;
+
+    [HideInInspector]
+    [SerializeField]
+    private double lhs;
+    [HideInInspector]
+    [SerializeField]
+    private double rhs;
+
+    private Equation() {}
+
+    public Equation(string equation)
+    {
+        if (!EquationRegex.IsMatch(equation))
+        {
+            isValid = false;
+            throw new ArgumentException("Invalid equation.");
+        }
+
+        string[] operands = equation.Split("+");
+
+        if (!double.TryParse(operands[0].Trim(), out double tempLhs))
+        {
+            isValid = false;
+            throw new ArgumentException("Invalid left-hand side operand");
+        }
+
+        if (!double.TryParse(operands[1].Trim(), out double tempRhs))
+        {
+            isValid = false;
+            throw new ArgumentException("Invalid right-hand side operand");
+        }
+
+        lhs = tempLhs;
+        rhs = tempRhs;
+
+        isValid = true;
+    }
+
+    public double Evaluate(params (string, double)[] variables)
+    {
+        if (!isValid)
+        {
+            throw new Exception("Equation is not properly initialized");
+        }
+
+        return lhs + rhs;
+    }
+}
---
+++ Assets/Scripts/ScriptableObejects/TestUpgrade.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TestUpgrade", menuName = "SO/Configuration/TestUpgrade")]
+public class TestUpgrade : Upgrade
+{
+    [SerializeField]
+    private Equation _baseValueEquation;
+    [SerializeField]
+    private Equation _costEquation;
+    [SerializeField]
+    private Equation _effectEquation;
+
+    [ContextMenu("Parse Equations")]
+    public void ParseEquations()
+    {
+        _baseValueEquation = new Equation(BaseValueEquation);
+        _costEquation = new Equation(CostEquation);
+        _effectEquation = new Equation(EffectEquation);
+    }
+
+    public override double GetCumulativeCost(int targetLevel)
+    {
+        double cost = 0;
+        for (int i = currentLevel; i < targetLevel; i++)
+        {
+            double lvlCost = _costEquation.Evaluate(("x", i));
+            cost += lvlCost;
+        }
+        return Math.Ceiling(cost);
+    }
+
+    public override void UpdateEffect(int level)
+    {
+        int multiplierValue = GetMultiplierForLevel(level);
+
+        if (EffectEquation != null)
+        {
+            currentEffect = _effectEquation.Evaluate(("x", multiplierValue));
+        }
+        else
+        {
+            currentEffect = 0;
+        }
+    }
+
+    public override void SetMultipliedBaseValue(int resetMultiplier) //after a reset upgrade buy
+    {
+        if (BaseValueEquation != null)
+        {
+            currentBaseValue = (int)_baseValueEquation.Evaluate(("k", resetMultiplier));
+        }
+    }
+}
