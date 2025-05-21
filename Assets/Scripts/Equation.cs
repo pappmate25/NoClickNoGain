c3564edc9f84731f9e5d2fad9482adbb39ff021d
Pap Sándor Dániel <pap.sandor.daniel@pm.me>
2025-05-21 13:45:25 +0200

    infix to rpn conversion works in equation class

--- Assets/Scripts/Equation.cs
+++ Assets/Scripts/Equation.cs
@@ -18,47 +18,106 @@ public struct Equation
 
     public Equation(string equation)
     {
-        if (!EquationRegex.IsMatch(equation))
+        isValid = false;
+        equationTokens = new EquationToken[] { };
+
+        try
         {
-            isValid = false;
-            throw new ArgumentException("Invalid equation.");
-        }
-
-        equation = equation.Replace(" ", string.Empty);
-
-        isValid = true;
-
-        var tokens = EquationRegex.Matches(equation);
-        equationTokens = tokens.ToList().Select((token) => new EquationToken(token.Value)).ToArray();
-
-        var outputQueue = new Queue<EquationToken>();
-        var operatorStack = new Stack<EquationToken>();
-
-        foreach (var token in equationTokens)
-        {
-            switch (token.TypeGroup)
+            if (!EquationRegex.IsMatch(equation))
             {
-                case TokenTypeGroup.Value:
-                    outputQueue.Enqueue(token);
-                    break;
-                case TokenTypeGroup.Operator:
-                    /*
-                    - an operator o1:
-                        while (
-                            there is an operator o2 at the top of the operator stack which is not a left parenthesis, 
-                            and (o2 has greater precedence than o1 or (o1 and o2 have the same precedence and o1 is left-associative))
-                        ): 
-                            pop o2 from the operator stack into the output queue
-                        push o1 onto the operator stack
-                    */
-                    
-
-                    break;
-                case TokenTypeGroup.Parenthesis:
-                    break;
+                isValid = false;
+                throw new ArgumentException("Invalid equation.");
             }
+
+            equation = equation.Replace(" ", string.Empty);
+
+
+            var tokens = EquationRegex.Matches(equation);
+            equationTokens = tokens.ToList().Select((token) => new EquationToken(token.Value)).ToArray();
+
+            // Shunting yard algorithm
+            // https://en.wikipedia.org/wiki/Shunting_yard_algorithm
+            var outputQueue = new Queue<EquationToken>();
+            var operatorStack = new Stack<EquationToken>();
+
+
+            foreach (var token in equationTokens)
+            {
+                switch (token.TypeGroup)
+                {
+                    case TokenTypeGroup.Value:
+                        outputQueue.Enqueue(token);
+                        break;
+                    case TokenTypeGroup.Operator:
+                        /*
+                        - an operator o1:
+                            while (
+                                there is an operator o2 at the top of the operator stack which is not a left parenthesis, 
+                                and (o2 has greater precedence than o1 or (o1 and o2 have the same precedence and o1 is left-associative))
+                            ): 
+                                pop o2 from the operator stack into the output queue
+                            push o1 onto the operator stack
+                        */
+
+                        while (operatorStack.Count() != 0 && ShouldPopOperatorFromStack(token, operatorStack.Peek()))
+                        {
+                            outputQueue.Enqueue(operatorStack.Pop());
+                        }
+                        operatorStack.Push(token);
+
+                        break;
+                    case TokenTypeGroup.Parenthesis:
+                        if (token.TokenType == EquationTokenType.LeftParenthesis)
+                        {
+                            operatorStack.Push(token);
+                        }
+                        else
+                        {
+                            while (true)
+                            {
+                                bool operatorsRemain = operatorStack.TryPeek(out var result);
+
+                                if (operatorsRemain && result.TokenType == EquationTokenType.LeftParenthesis)
+                                {
+                                    // Pop left parenthesis
+                                    operatorStack.Pop();
+                                    break;
+                                }
+
+                                if (!operatorsRemain)
+                                {
+                                    throw new Exception("No matching left parenthesis found for right parenthesis.");
+                                }
+
+                                outputQueue.Enqueue(operatorStack.Pop());
+                            }
+                        }
+                        break;
+                }
+            }
+
+            while (operatorStack.TryPop(out var token))
+            {
+                if (token.TokenType == EquationTokenType.LeftParenthesis)
+                {
+                    throw new Exception("No matching right parenthesis found for left parenthesis.");
+                }
+
+                outputQueue.Enqueue(token);
+            }
+
+            equationTokens = outputQueue.ToArray();
+            isValid = true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"{e.StackTrace}");
         }
     }
+
+    static bool ShouldPopOperatorFromStack(EquationToken o1, EquationToken o2) =>
+            o2.TokenType != EquationTokenType.LeftParenthesis
+            && (o2.ComparePrecedence(o1) == 1 || (o2.ComparePrecedence(o1) == 0 && o1.IsLeftAssociative()));
 
     public double Evaluate(params (string, double)[] variables)
     {
@@ -74,7 +133,7 @@ public struct Equation
         return 1;
     }
 
-    private void checkVariables((string, double)[] variables)
+    private readonly void checkVariables((string, double)[] variables)
     {
         var variableTokens = equationTokens
             .Where(token => token.TokenType == EquationTokenType.Variable)
@@ -131,15 +190,26 @@ public struct EquationToken
     /// <para>0 this operator equals the other operator in precedence.</para>
     /// <para>1 this operator is higher in precedence than the other one.</para>
     /// </returns>
-    public int ComparePrecedence(EquationToken other)
+    public readonly int ComparePrecedence(EquationToken other)
     {
-        if (TypeGroup != TokenTypeGroup.Operator || other.TypeGroup != TokenTypeGroup.Operator)
-            throw new ArgumentException("Compared type");
+        if (TypeGroup != TokenTypeGroup.Operator)
+            throw new ArgumentException("This token is not an operator but an attempt was made to compare it in precedence.");
+
+        if (other.TypeGroup != TokenTypeGroup.Operator)
+            throw new ArgumentException("The token this was compared to in precedence is not an operator.");
 
         int thisPrecedenceRank = (int)TokenType / 10;
         int otherPrecedenceRank = (int)other.TokenType / 10;
 
-        return Math.Sign(otherPrecedenceRank - thisPrecedenceRank);
+        return Math.Sign(thisPrecedenceRank - otherPrecedenceRank);
+    }
+
+    public readonly bool IsLeftAssociative()
+    {
+        if (TypeGroup != TokenTypeGroup.Operator)
+            throw new ArgumentException("This token is not an operator but an attempt was made to check its associativity.");
+
+        return (int)TokenType % 10 < 5;
     }
 
     public EquationToken(string tokenString)
@@ -199,8 +269,11 @@ public enum EquationTokenType
     /*
     Lower number means higher precedence.
     Precedence is understood in groups of ten, therefore 10 and 11 count as the same precedence.
+
+    Using modulo 10 you can determine the associativity of an operator. 
+    If modulo 10 results in a number smaller than 5, then the operator is left associative, if not it is right associative.
     */
-    Exponentiation = 0,
+    Exponentiation = 5,
     Addition = 10,
     Subtraction = 11,
     Multiplication = 20,
