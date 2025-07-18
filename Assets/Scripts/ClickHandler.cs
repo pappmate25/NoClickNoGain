e0db14a503acd960cd825268386aa7d59a67c4d2
pappmateSyn <mate.papp@kritigames.com>
2025-07-18 12:56:49 +0200

    click value popup

--- Assets/Scripts/ClickHandler.cs
+++ Assets/Scripts/ClickHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -49,9 +50,63 @@ public class ClickHandler : MonoBehaviour
 
         if (picked == null && UIController.isClaimed)
         {
+            double clickValue = GameController.Instance.GetClickValue();
             ClickEvent.Raise(NoDetails.Instance);
             ClickCounter++;
             Debug.Log($"Total clicks: {ClickCounter}");
+
+            ShowGainValue(screenPosition, clickValue);
         }
+    }
+
+    private void ShowGainValue(Vector2 position, double gain)
+    {
+        position.y = rootElement.resolvedStyle.height - position.y;
+
+
+        //Random X|Y offset
+        //float yOffset = Random.Range(-200f, 200f);
+        //float xOffset = Random.Range(-300f, 250f);
+        //position.y += yOffset;
+        //position.x += xOffset;
+
+
+        Label clickPopUpLabel = new Label($"+{NumberFormatter.FormatNumber(gain)}");
+        clickPopUpLabel.AddToClassList("clickPopUpLabelStyle");
+        clickPopUpLabel.pickingMode = PickingMode.Ignore;
+
+        rootElement.Add(clickPopUpLabel);
+
+        StartCoroutine(ShowGainFloatingAnimation(clickPopUpLabel, position));
+    }
+
+
+    //without the random offset:
+        //duration = 1.0f;
+        //float offsetY = -275f * progress; // Upwards floating
+
+    //with the random offset:
+        //duration = 1.5f;
+        //float offsetY = -50f * progress; // Upwards floating
+    private IEnumerator ShowGainFloatingAnimation(Label label, Vector2 startPos)
+    {
+        float duration = 1.5f;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float progress = elapsed / duration;
+
+            float offsetY = -325f * progress; // Upwards floating
+            float opacity = 1.1f - progress;
+
+            label.style.translate = new StyleTranslate(new Translate(startPos.x + 15f, startPos.y -40f + offsetY, 0)); //a bit up and right from the click position --> without random offset --> startPos.x + 15f, startPos.y -40f + offsetY
+            label.style.opacity = opacity;
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        rootElement.Remove(label);
     }
 }
