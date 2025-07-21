50ea18feaa6732499cb470ce71d031e6a9cacfb1
Pap Sándor Dániel <pap.sandor.daniel@pm.me>
2025-07-21 15:46:05 +0200

    implemented audio controller

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip buyQuantitySwap;
+    [SerializeField]
+    private AudioClip closeSkills;
+    [SerializeField]
+    private AudioClip openSkills;
+    [SerializeField]
+    private AudioClip resetPassiveSkillBuy;
+    [SerializeField]
+    private AudioClip resetShower;
+    [SerializeField]
+    private AudioClip swapSkillTabWhileOpen;
+    [SerializeField]
+    private AudioClip upgradeSkills;
+    [SerializeField]
+    private AudioClip welcomeBackClaimed;
+
+    public static AudioController Instance { get; private set; }
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(SfxType sfxType)
+    {
+        AudioClip clip = sfxType switch
+        {
+            SfxType.BuyQuantitySwap => buyQuantitySwap,
+            SfxType.CloseSkills => closeSkills,
+            SfxType.OpenSkills => openSkills,
+            SfxType.ResetPassiveSkillBuy => resetPassiveSkillBuy,
+            SfxType.ResetShower => resetShower,
+            SfxType.SwapSkillTabWhileOpen => swapSkillTabWhileOpen,
+            SfxType.UpgradeSkills => upgradeSkills,
+            SfxType.WelcomeBackClaimed => welcomeBackClaimed,
+            _ => throw new System.ArgumentOutOfRangeException(nameof(sfxType), $"No sound defined for {sfxType}"), 
+        };
+
+        audioSource.PlayOneShot(clip);
+    }
+}
+
+public enum SfxType
+{
+    BuyQuantitySwap,
+    CloseSkills,
+    OpenSkills,
+    ResetPassiveSkillBuy,
+    ResetShower,
+    SwapSkillTabWhileOpen,
+    UpgradeSkills,
+    WelcomeBackClaimed
+}
