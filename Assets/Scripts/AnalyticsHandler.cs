6c968c860d2e726ceaed1bf645d7f9f548ca5019
Pap Sándor Dániel <pap.sandor.daniel@pm.me>
2025-08-27 16:07:07 +0200

    analytics handler can collect events

--- Assets/Scripts/AnalyticsHandler.cs
+++ Assets/Scripts/AnalyticsHandler.cs
@@ -7,38 +7,72 @@ public class AnalyticsHandler : MonoBehaviour
 {
     private List<AnalyticsEvent> events;
     
+    private void Awake()
+    {
+        events = new List<AnalyticsEvent>();
+    }
+
     public void OnEvent(IGameEventDetails gameEventDetails)
     {
-        var newEvent = gameEventDetails switch
+        Debug.Log(gameEventDetails.GetType().Name);
+        
+        AnalyticsEvent? newEvent = gameEventDetails switch
         {
-            GainChangedEventDetails gainChangedEventDetails => new AnalyticsEvent
+            GainChangedEventDetails gainChangedEventDetails => new AnalyticsEvent(EventType.GainChanged,
+            new[]
             {
-                EventType = EventType.GainChanged,
-                Parameters = new[]
-                {
-                    ("GainChangeSource", Enum.GetName(typeof(GainChangeType), gainChangedEventDetails.ChangeType)),
-                    ("NewGain", gainChangedEventDetails.NewGain.ToString(CultureInfo.InvariantCulture))
-                }
-            },
-            PassiveSkillBought passiveSkillBought => throw new NotImplementedException(),
-            ResetEventDetails resetEventDetails => throw new NotImplementedException(),
-            ResetUpgradeBought resetUpgradeBought => throw new NotImplementedException(),
-            UpgradeBought upgradeBought => throw new NotImplementedException(),
+                ("NewGain", gainChangedEventDetails.NewGain.ToString(CultureInfo.InvariantCulture)),
+                ("GainSource", Enum.GetName(typeof(GainChangeType), gainChangedEventDetails.ChangeType))
+            }),
+            PassiveSkillBought passiveSkillBought => new AnalyticsEvent(EventType.PassiveSkillBought, new[]
+            {
+                ("PassiveSkill", passiveSkillBought.PassiveSkill.Name)
+            }),
+            ResetEventDetails resetEventDetails => new AnalyticsEvent(EventType.Reset, new[]
+            {
+                ("GainAtTimeOfReset", resetEventDetails.GainOnReset.ToString(CultureInfo.InvariantCulture))
+            }),
+            ResetUpgradeBought resetUpgradeBought => new AnalyticsEvent(EventType.ResetUpgradeBought, new[]
+            {
+                ("ResetUpgrade", resetUpgradeBought.ResetUpgrade.Name)
+            }),
+            UpgradeBought upgradeBought => new AnalyticsEvent(EventType.UpgradeBought, new[]
+            {
+                ("Upgrade", upgradeBought.Upgrade.Name),
+                ("TargetLevel", upgradeBought.TargetLevel.ToString())
+            }),
             _ => throw new ArgumentOutOfRangeException(nameof(gameEventDetails), "Unhandled event type")
         };
-        
-        newEvent.Timestamp = DateTimeOffset.Now;
-        
-        events.Add(newEvent);
+
+        if (newEvent == null) return;
+
+        var savedEvent = newEvent.Value;
+        events.Add(savedEvent);
+    }
+    
+    [ContextMenu("Print All Analytics Events")]
+    public void PrintAllEvents()
+    {
+        foreach (var analyticsEvent in events)
+        {
+            Debug.Log(analyticsEvent.ToString());
+        }
     }
 }
 
 public struct AnalyticsEvent
 {
-    public DateTimeOffset Timestamp;
-    public Enum EventType;
-    public (string, string)[] Parameters;
-    
+    public readonly DateTimeOffset Timestamp;
+    public readonly Enum EventType;
+    public readonly (string, string)[] Parameters;
+
+    public AnalyticsEvent(EventType eventType, (string, string)[] parameters)
+    {
+        EventType = eventType;
+        Parameters = parameters;
+        Timestamp = DateTimeOffset.Now;
+    }
+
     public override string ToString()
     {
         var parametersString = string.Join(", ", Array.ConvertAll(Parameters, p => $"{p.Item1}: {p.Item2}"));
